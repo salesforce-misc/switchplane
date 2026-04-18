1f07944d37c93477be36119612c3033ea986cf4a
Demian Brecht <demianbrecht@gmail.com>
2026-04-17 17:39:28 -0700

    update stale url

--- src/switchplane/__main__.py
+++ src/switchplane/__main__.py
@@ -1,4 +1,4 @@
 """Switchplane is used as a library. Create an Application and call app.run()."""
 
 print("Switchplane is a library. Create an Application and call app.run().")
-print("See: https://github.com/dbrecht/switchplane")
+print("See: https://github.com/salesforce-misc/switchplane")
